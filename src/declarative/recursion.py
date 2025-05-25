d9942e8793082d188a2046caf8795aaad658d2c7
MohamedAbdElGani <mohamed.kk878@gmail.com>
2025-05-25 06:16:13 +0300

    Added a `recursion` module with factorial implementation

---
+++ src/declarative/recursion.py
@@ -0,0 +1,13 @@
+"""Module for recursion and visualization for it."""
+
+def factorial(num: int) -> int:
+    """
+    Calculate factorial of a given number.
+    Args:
+        num (int): Number to calculate factorial for.
+    Returns:
+        int: Factorial of the given number.
+    """
+    if num == 0:
+        return 1
+    return num * factorial(num - 1)
