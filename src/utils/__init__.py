627f5905988926b9541bb655f45159d14bb5c502
MohamedAbdElgani <mohamed.kk878@gmail.com>
2025-08-02 06:26:52 +0300

    Exported debug utilities and added `__version__` to `__init__.py`.
    Signed-off-by: MohamedAbdElgani <mohamed.kk878@gmail.com>

---
+++ src/utils/__init__.py
@@ -0,0 +1,10 @@
+from .debug_utils import DEBUG, debug_print, set_debug, is_debug_enabled
+
+__all__ = [
+    'DEBUG',
+    'debug_print',
+    'set_debug',
+    'is_debug_enabled'
+]
+
+__version__ = '1.0.0'
